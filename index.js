81d058824c4b2290e4d83ea22a4a8ee5ba9a8b33
Kevin <klums24@gmail.com>
2023-04-01 18:16:20 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push ("Thank you, " + names[i] + ", for the wonderful " + eventName + " gift!");
+    }
+    return messages;
+    
+}
+
+writecards(["Guadalupe", "Ollie", "Aki", "surprise"]);
+
+//countdown
+function countDown(number){
+    let i = 0;
+    while (i <= number){
+        console.log(number);
+        number--;
+    }
+    
+}
+
+countDown(10);
